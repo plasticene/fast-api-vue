1ab538e48d653b5537b9f63c12aff6c13c49346a
fjzheng <1059959730@qq.com>
2022-10-21 18:44:35 +0800

    add:  :rocket:  初步添加数据源组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <data-source></data-source>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DataSource from '@/views/metadata/DataSource.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DataSource,
   }
 }
 </script>
---
+++ src/views/metadata/DataSource.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>我是数据源</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"DataSource",
+
+}
+</script>
+
+<style>
+
+</style>
