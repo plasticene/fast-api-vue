1de02d457523d229019a2f024f489a220cd0395f
fjzheng <1059959730@qq.com>
2022-10-24 16:54:34 +0800

    add: :rocket:  添加菜单信息数据封装

---
+++ src/store/modules/menuBar.js
@@ -0,0 +1,21 @@
+/*
+菜单栏数据
+**/
+
+const state = {
+    isCollapse:false,
+    tabsList: [
+        {
+            path: "/",
+            name: "home",
+            label: "首页",
+            icon: "s-home",
+            url: "Home/Home",
+        }
+    ], // 面包屑数据
+    menuList:[
+        {
+            
+        }
+    ]
+}
