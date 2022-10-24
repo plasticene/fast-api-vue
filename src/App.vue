fe5aff6854e3932c7e0840f05e1bf822872cabea
fjzheng <1059959730@qq.com>
2022-10-24 16:51:05 +0800

    update: :rocket: 测试布局

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <div id="app">
-    <data-source></data-source>
+  <div>
+    <app-main></app-main>
   </div>
 </template>
 
 <script>
 import DataSource from '@/views/metadata/DataSource.vue'
+import AppMain from '@/layout/AppMain.vue'
 export default {
   name: 'App',
   components: {
     DataSource,
+    AppMain
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  html,body,h3, p {
+    margin: 0;
+    padding: 0
+  }
 </style>
