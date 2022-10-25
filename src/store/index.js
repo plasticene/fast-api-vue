46f428761fc2bd51b51f89e9187865fd2197134d
fjzheng <1059959730@qq.com>
2022-10-25 22:25:51 +0800

    add:  :rocket: 添加store菜单信息公共数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import menubar from './modules/menuBar'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules:{
-        
+        menubar
     }
 
 })
--- src/store/modules/menuBar.js
+++ src/store/modules/menuBar.js
@@ -15,7 +15,97 @@ const state = {
     ], // 面包屑数据
     menuList:[
         {
-            
+            path:"/",
+            name:"home",
+            title:"首页",
+            icon:"s-home"
+        },
+        {
+            name:"metadata",
+            title:"元数据",
+            icon:"s-home",
+            children:[
+                {
+                    path:"/dataSource",
+                    name:"dataSource",
+                    title:"数据源",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/table",
+                    name:"talble",
+                    title:"表管理",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/sqlEditor",
+                    name:"sqlEditor",
+                    title:"SQL编辑器",
+                    icon:"s-home" 
+                }
+            ]
+        },
+        {
+            name:"apiManange",
+            title:"api管理",
+            icon:"s-home",
+            children:[
+                {
+                    path:"/apiHouse",
+                    name:"apihouse",
+                    title:"api库",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/apiAuth",
+                    name:"apiAuth",
+                    title:"接口权限",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/apiVisit",
+                    name:"apiVist",
+                    title:"访问记录",
+                    icon:"s-home" 
+                }
+            ]
+        },
+        {
+            name:"system",
+            title:"系统管理",
+            icon:"s-home",
+            children:[
+                {
+                    path:"/user",
+                    name:"user",
+                    title:"用户管理",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/role",
+                    name:"role",
+                    title:"角色管理",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/menu",
+                    name:"menu",
+                    title:"菜单管理",
+                    icon:"s-home" 
+                },
+                {
+                    path:"/dept",
+                    name:"dept",
+                    title:"部门管理",
+                    icon:"s-home" 
+                },
+            ]
         }
     ]
+}
+
+export default {
+    // namespaced:true,
+    state,
+    
 }
