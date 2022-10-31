9a9613d93ac204a0e9263c3d69aca835a9598acf
fjzheng <1059959730@qq.com>
2022-10-31 18:20:52 +0800

    add: 添加全局组件dictTag

---
+++ src/components/dictTag/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+      <template v-for="(dict, index) in dictEnum">
+
+        <span style="color:#13227a" v-if ="value === dict.value" :key="dict.value" :index="index" :class="dict.cssClass">{{ dict.label }}</span>
+
+      </template>
+    </span>
+  </template>
+  
+  <script>
+  export default {
+    name: "DictTag",
+    props:['value', 'dictEnum'],
+  };
+  </script>
+  <style scoped>
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  </style>
+  
