91a0f87425a525e9766f8fa1d9084da0a778ce69
fjzheng <1059959730@qq.com>
2022-10-31 22:34:30 +0800

    add: :rocket: 数据源组件添加右侧工具栏

--- src/views/metadata/dataSource/index.vue
+++ src/views/metadata/dataSource/index.vue
@@ -40,7 +40,7 @@
           <el-button type="warning" icon="el-icon-download" size="mini" @click="handleExport" :loading="exportLoading"
                      >导出</el-button>
         </el-col>
-        <!-- <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar> -->
+        <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
       </el-row>
 
       <el-table v-loading="loading" :data="dataSourceList">
