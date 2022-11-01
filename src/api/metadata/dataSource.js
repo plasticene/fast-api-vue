dcfd4f49d43337b085d0c8d12a6b816ac108e411
fjzheng <1059959730@qq.com>
2022-11-01 17:40:53 +0800

    add: :rocket: 添加数据源后端接口

--- src/api/metadata/dataSource.js
+++ src/api/metadata/dataSource.js
@@ -1,10 +1,38 @@
 import request from '@/utils/request'
 
+// 获取数据源列表接口
 export function getDataSourceList(query) {
     return request({
         url: '/fds/dataSource',
         method: 'get',
         params: query
     })
-
 }
+
+// 检测数据源是否可以连接
+export function checkConnection(data) {
+    return request({
+        url: '/fds/dataSource/test',
+        method: 'post',
+        data: data
+      })
+}
+
+// 添加数据源
+export function addDataSource(data) {
+    return request({
+        url: '/fds/dataSource',
+        method: 'post',
+        data: data
+      })
+}
+
+// 更新数据源
+export function updateDataSource(data) {
+    return request({
+        url: '/fds/dataSource' + data.id,
+        method: 'put',
+        data: data
+      })
+}
+
