50eb731940848b0315f6dc09474a1ac9b50b0746
fjzheng <1059959730@qq.com>
2022-11-02 16:24:27 +0800

    update: :fire: 注释掉之前在console打印的接口日志

--- src/utils/request.js
+++ src/utils/request.js
@@ -44,7 +44,7 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-    console.log('res', res)
+    // console.log('res', res)
 
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 200) {
