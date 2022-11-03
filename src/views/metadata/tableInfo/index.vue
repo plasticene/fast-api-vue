183ae3269b679e5b5f456961939ac94666459d9e
fjzheng <1059959730@qq.com>
2022-11-03 13:50:39 +0800

    update: :art: 初步实现表信息组件布局

--- src/views/metadata/tableInfo/index.vue
+++ src/views/metadata/tableInfo/index.vue
@@ -1,16 +1,123 @@
 <template>
-  <div>
-    <h1>我是表管理组件</h1>
+  <div class="components-container">
+    <div>
+      <h3>表信息</h3>
+    </div>
+    <split-pane split="vertical" :min-percent='15' :default-percent='20' @resize="resize">
+      <template slot="paneL">
+        <div class="left-container">
+          <el-input
+            placeholder="输入关键字进行过滤"
+            v-model="filterText">
+          </el-input>
+
+          <el-tree
+            class="filter-tree"
+            :data="data"
+            :props="defaultProps"
+            default-expand-all
+            :filter-node-method="filterNode"
+            ref="tree">
+          </el-tree>
+        </div>
+      </template>
+      <template slot="paneR">
+        <div class="right-container" />
+      </template>
+    </split-pane>
+    
   </div>
 </template>
 
 <script>
+import splitPane from 'vue-splitpane'
 export default {
-    name:'TableInfo'
+    name:'TableInfo',
+    components: {
+      splitPane
+    },
+    data() {
+      return {
+        filterText: '',
+        data: [{
+          id: 1,
+          label: '一级 1',
+          children: [{
+            id: 4,
+            label: '二级 1-1',
+            children: [{
+              id: 9,
+              label: '三级 1-1-1'
+            }, {
+              id: 10,
+              label: '三级 1-1-2'
+            }]
+          }]
+        }, {
+          id: 2,
+          label: '一级 2',
+          children: [{
+            id: 5,
+            label: '二级 2-1'
+          }, {
+            id: 6,
+            label: '二级 2-2'
+          }]
+        }, {
+          id: 3,
+          label: '一级 3',
+          children: [{
+            id: 7,
+            label: '二级 3-1'
+          }, {
+            id: 8,
+            label: '二级 3-2'
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      }
+    },
+
+    watch: {
+      filterText(val) {
+        this.$refs.tree.filter(val);
+      }
+    },
+
+    methods: {
+      resize() {
+        console.log('resize')
+      },
+
+      filterNode(value, data) {
+        if (!value) return true;
+        return data.label.indexOf(value) !== -1;
+      }
+  }
 
 }
 </script>
 
-<style>
+<style  scoped>
+  .components-container {
+    position: relative;
+    height: 100vh;
+    margin-left: 10px;
+  }
+
+  .left-container {
+    /* background-color: #F38181; */
+    height: 100%;
+    width: 100%;
+  }
+
+  .right-container {
+    background-color: #FCE38A;
+    width: 100%;
+    height: 100%;
+  }
 
 </style>
