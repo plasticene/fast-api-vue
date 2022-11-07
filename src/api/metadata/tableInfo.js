f558a100f6f7953f5d8689ecef8b876cf7e42780
fjzheng <1059959730@qq.com>
2022-11-07 13:54:26 +0800

    add: :rocket: 添加表信息相关接口

---
+++ src/api/metadata/tableInfo.js
@@ -0,0 +1,27 @@
+// 其实这里的接口也是数据源下的，放在dataSource.js下也是可以的，但是为了方便维护，清楚了然，组件和接口js文件一一对应我们就放到这里
+
+import request from '@/utils/request'
+
+export function getTableList(data) {
+    return request({
+        url: '/fds/dataSource/database/table',
+        method: 'get',
+        params: data
+    })
+}
+
+export function getTableStruct(data) {
+    return request({
+        url: '/fds/dataSource/table/struct',
+        method: 'get',
+        params: data
+    })
+}
+
+export function getTableData(data) {
+    return request({
+        url: '/fds/dataSource/table/data',
+        method: 'get',
+        params: data,
+    })
+}
