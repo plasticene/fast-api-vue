2b6beb6dbcba30a598308aa0a101fff8dbb1d704
fjzheng <1059959730@qq.com>
2022-11-10 10:45:19 +0800

    add: :rocket:  完善sql查询列表功能逻辑

--- src/views/metadata/sqlQuery/index.vue
+++ src/views/metadata/sqlQuery/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="82px">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="40px">
       <el-form-item label="名称" prop="name">
         <el-input v-model="queryParams.name" placeholder="请输入SQL查询" clearable @keyup.enter.native="handleQuery"/>
       </el-form-item>
 
-      <el-form-item label="名称" prop="folderId">
-        <el-input v-model="queryParams.folderId" placeholder="所属分组" clearable @keyup.enter.native="handleQuery"/>
+      <el-form-item label="分组" prop="folderId">
+        <el-select v-model="queryParams.folderId" placeholder="所属分组" clearable @keyup.enter.native="handleQuery">
+                <el-option v-for="folder in folderList" :key="folder.id" :label="folder.name" :value="folder.id"/>
+            </el-select>
       </el-form-item>
 
       <el-form-item>
@@ -23,7 +25,7 @@
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="sqlQueryList" @selection-change="handleSelectionChange">
+    <el-table v-loading="loading" :data="tableList" @selection-change="handleSelectionChange">
         <el-table-column type="selection" width="55" />
         <el-table-column label="序号" align="center" type="index" />
         <el-table-column label="名称" align="center" prop="name" />
@@ -32,29 +34,32 @@
         <el-table-column label="分组" align="center" prop="folderName" />
         <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" icon="el-icon-view" @click="handleUpdate(scope.row)"></el-button>
+            <el-button size="mini" type="primary" icon="el-icon-view" @click="showDetail(scope.row)"></el-button>
             <el-button size="mini" type="primary" icon="el-icon-edit" @click="handleUpdate(scope.row)"></el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete" @click="handleDelete(scope.row)"></el-button>
           </template>
         </el-table-column>
       </el-table>
-
       <pagination v-show="total>0" :total="total" :page.sync="queryParams.pageNo" :limit.sync="queryParams.pageSize"
                   @pagination="getList"/>
   </div>
 </template>
 
 <script>
+import { getSqlQueryList } from '@/api/metadata/sqlQuery';
+import { getFolderListByType } from '@/api/folder/folder';
 export default {
     name:'SqlQuery',
     data() {
       return {
 
         // 遮罩层
-        loading: false,
+        loading: true,
         showSearch:true,
-        sqlQueryList:[],
-        selections:[],
+        tableList:[],
+        total: 0,
+        selections: [],
+        folderList: [],
         // 查询参数
         queryParams: {
           pageNo: 1,
@@ -65,26 +70,54 @@ export default {
       }
     },
 
+    created() {
+      this.getList()
+      this.getFolderList()
+    },
+
     methods: {
       handleSelectionChange(val) {
         this.selections = val
       },
-      
+
       getList() {
+        this.loading = true;
+        getSqlQueryList(this.queryParams).then(response => {
+          this.tableList = response.data.list;
+          this.total = response.data.total;
+          this.loading = false;
+        });
 
       },
 
       handleQuery() {
-
+        this.queryParams.pageNo = 1;
+        this.getList();
       },
-
+      
       resetQuery() {
-
+        this.resetForm("queryForm");
+        this.handleQuery();
       },
 
       handleAdd() {
         this.$router.push({ path: "/metadata/sqlQuery/sqlEditor"});
       },
+      
+      handleUpdate(row) {
+        this.$router.push({ path: "/metadata/sqlQuery/sqlEditor", query: {id: row.id}});
+      },
+
+      showDetail(row) {
+        this.$router.push({ path: "/metadata/sqlQuery/sqlDetail", query: {id: row.id}});
+      },
+
+      getFolderList() {
+        getFolderListByType(0).then(response => {
+          this.folderList = response.data;
+        })
+
+      }
 }
 }
 </script>
