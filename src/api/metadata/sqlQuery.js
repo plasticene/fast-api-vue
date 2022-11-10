940feb56ac9acbdded89c198db5f8f3de11e620f
fjzheng <1059959730@qq.com>
2022-11-10 10:42:30 +0800

    add: :rocket:  添加SQL查询相关接口

--- src/api/metadata/sqlQuery.js
+++ src/api/metadata/sqlQuery.js
@@ -14,5 +14,29 @@ export function addSqlQuery(data) {
         method: 'post',
         data: data
     })
+}
+
+export function updateSqlQuery(data) {
+    return request({
+        url: '/fds/sql/query/' + data.id,
+        method: 'put',
+        data: data
+    })
 
 }
+
+export function getSqlQueryList(query) {
+    return request({
+        url: '/fds/sql/query',
+        method: 'get',
+        params: query
+    })
+}
+
+export function getSqlQueryDetail(id) {
+    return request({
+        url: '/fds/sql/query/' + id,
+        method: 'get',
+    })
+}
+
