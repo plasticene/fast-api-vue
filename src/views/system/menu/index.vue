600cc71f04c70620242c6df7ae4b78184bc9d011
fjzheng <1059959730@qq.com>
2023-02-01 10:38:11 +0800

    add: :rocket: 添加菜单vue

---
+++ src/views/system/menu/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>我是菜单vue</h1>
+</template>
+
+<script>
+export default {
+    name:'Menu',
+    data() {
+        return {
+            
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
