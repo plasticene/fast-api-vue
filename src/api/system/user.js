e812e50f7be8c567f5d9dd9570c39334fdcbea75
fjzheng <1059959730@qq.com>
2023-02-13 13:54:17 +0800

    add: :rocket: 添加用户相关接口

--- src/api/system/user.js
+++ src/api/system/user.js
@@ -7,3 +7,34 @@ export function getUserList(query) {
     params: query
   })
 }
+
+export function addUser(data) {
+  return request({
+    url: 'fds/user',
+    method: 'post',
+    data: data
+  })
+}
+
+export function updateUser(data) {
+  return request({
+    url: 'fds/user/' + data.id,
+    method: 'put',
+    data: data
+  })
+}
+
+export function deleteUser(id) {
+  return request({
+    url: 'fds/user/' + id,
+    method: 'delete'
+  })
+}
+
+export function assignUser(data) {
+  return request({
+    url: '/fds/user/role',
+    method: 'post',
+    data: data
+  })
+}
